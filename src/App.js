16a15f18af903373cff2821f9418b0b8b2d9d222
Dewayne Sewell <dewaynesewell1@gmail.com>
2022-12-05 21:38:26 +0000

    reactrouter

--- src/App.js
+++ src/App.js
@@ -5,23 +5,37 @@ import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
 import PokemonMovesSelector from "./PokemonMovesSelector";
 import PokemonCity from "./PokemonCity";
+import PokemonInfo from "./PokemonInfo";
+import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom";
 
 function App (props)  {
 const userName="Dewayne";
  const abilities = ["Anticipation","Adaptability","Run-Away"];
  let date= new Date().toLocaleDateString();
+ let { name } = useParams();
+ 
   return (
+
     <div className="App">
       <header>
        <h1>Welcome to {userName}'s</h1>
        <Logo appName="Pokedex" />
       </header>
-     <BestPokemon abilities = {abilities} pokemon = {"hello Pickachu"}  />
-     <CaughtPokemon date ={date} />
+       <BrowserRouter>
+       <Link to="src/BestPokemon.js">Best Pokemon</Link>
+       <Link to="src/CaughtPokemon.js">Caught Pokemon</Link>
      <PokemonMovesSelector />
      <PokemonCity />
+     <Routes> 
+      <Route element={<BestPokemon abilities = {abilities} pokemon = {"hello Pickachu"}  />} path="src/BestPokemon.js" />
+      <Route element={<CaughtPokemon date ={date} />} path="src/CaughtPokemon.js" />
+      <Route path="/pokemon/:name" element={<PokemonInfo />} />
+     </Routes>
+      </BrowserRouter>
     </div>
   );
+
+  
 }
 
 
---
+++ src/exercise.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class exercise extends Component {
+  constructor() {
+    super();
+    this.state={
+      randomUser:[]
+    }
+  }
+
+    componentDidMount(){
+        fetch(" ")
+    .then((res) => res.json())
+    .then((data) => {
+      
+    });
+
+    }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
